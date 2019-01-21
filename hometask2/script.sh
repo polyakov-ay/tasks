bde6688b34b47f3e1b57af7ce8fd5d824585c6f8
Serhii Tkachuk <tkachuk42@gmail.com>
2019-01-21 22:01:16 +0200

    Some changes

--- hometask2/script.sh
+++ hometask2/script.sh
@@ -8,8 +8,9 @@ else
 	mkdir /tmp/tar;
 	echo "Директория /tmp/tar успешно создана";
 fi
-cd /
+
 cd /tmp/tar/
+
 wget https://netcologne.dl.sourceforge.net/project/od1n/samples.tar.gz -O - | tar -xz
 cd samples/
 echo "файлов в архиве:"; ls | wc -l
